ab8161d60b01a04fc526c5bf5434cf87c151a9cf
brunomanente <bruno.manente0@gmail.com>
2020-03-31 21:16:32 -0300

    jogo final

---
+++ Assets/Scripts/CheckPointController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Classe responsavel por controllar as voltas do jogador.
+/// </summary>
+/// 
+
+public class CheckPointController : MonoBehaviour
+{
+    public GameController gameController;
+
+    private Transform[] checkpoints;
+
+    private int maxCheckPoints = 0;
+    private int checkpointAtual = 0;
+
+    private int voltaAtual = 0;
+
+    private int maximoVoltas;
+
+    private void Awake()
+    {
+        GameObject checkpointContainer = GameObject.FindGameObjectWithTag("CheckpointsContainer");
+
+        checkpoints = new Transform[checkpointContainer.transform.childCount];
+        for (int i = 0; i < checkpointContainer.transform.childCount; i++)
+        {
+            checkpoints[i] = checkpointContainer.transform.GetChild(i);
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maximoVoltas = gameController.maximoVoltas;
+        maxCheckPoints = checkpoints.Length - 1;
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Checkpoint")) {
+            if (other.transform == checkpoints[checkpointAtual])  {
+                if (checkpointAtual < maxCheckPoints)  {
+                    if (checkpointAtual == 0) {   
+                    
+                        if (voltaAtual == maximoVoltas) {
+                            gameController.FinishRace(gameObject);
+                        }
+                        else
+                        {
+                            voltaAtual++;
+                            gameController.UpdateVoltas(voltaAtual);
+                        }
+                    }
+                    checkpointAtual++;
+                }
+            
+                   else
+                  {
+                     checkpointAtual = 0;
+
+                   }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] players;
+
+    public Text HudPrincipal;
+
+    public Text hudVoltas;
+
+    public int voltaAtual = 0;
+
+    public int maximoVoltas = 5;
+
+
+    private float timer;
+
+    private float delay = 1f;
+
+    private int contador = 4;
+
+          
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time;
+
+        foreach (GameObject m_gameObject in players)
+        {
+            m_gameObject.GetComponent<PlayerController>().enabled = false;
+
+        }
+        hudVoltas.text = string.Format("{0}/{1}", voltaAtual, maximoVoltas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (contador >= 0)
+        {
+            if(delay < (Time.time - timer))
+            {
+                timer = Time.time;
+                contador--;
+            }
+
+            switch (contador)
+            {
+                case 4:
+                    HudPrincipal.text = "Super Coutry Race";
+                    break;
+                case 3:
+                    HudPrincipal.text = "Ready!";
+                    break;
+                case 2:
+                    HudPrincipal.text = "Set!";
+                    break;
+                case 1:
+                    HudPrincipal.text = "GO!";
+
+                foreach(GameObject m_gameObject in players)
+                {
+                        m_gameObject.GetComponent<PlayerController>().enabled = true;
+                }
+                    break;
+                case 0:
+
+                    HudPrincipal.text = "";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    public void UpdateVoltas(int voltaPlayer) {
+        if(voltaPlayer > voltaAtual)
+        {
+            voltaAtual = voltaPlayer;
+            hudVoltas.text = string.Format("{0}/{1}", voltaAtual, maximoVoltas);
+        }
+
+    }
+
+    /// <summary>
+    /// Funcao que exibe quem foi o ganhador da partida
+    /// </summary>
+    /// <param name="winner">GameOgbject do ganhador da partida</param>
+    
+    public void FinishRace(GameObject winner)
+    {
+        foreach (GameObject m_gameObject in players)
+        {
+            if (m_gameObject == winner)
+                m_gameObject.GetComponent<PlayerController>().enabled = false;
+            else
+                m_gameObject.SetActive(false);
+
+
+        }
+
+        HudPrincipal.text = string.Format("PlayerController {0} ganhou!!!", winner.GetComponent<PlayerController>().playerID);
+                  
+
+    }
+
+}
